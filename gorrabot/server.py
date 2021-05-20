335c0fc4b6631c0c396506c82acd97979c270f19
Ciro Goyeneche <soycirog@gmail.com>
2021-05-20 15:14:51 -0300

    [UPD] Gorrabot logic to detect changelog behaviour

--- gorrabot/server.py
+++ gorrabot/server.py
@@ -188,8 +188,8 @@ def check_status(mr_json: dict, project_name: str) -> NoReturn:
     (project_id, iid) = (mr_attributes['source_project_id'], mr_attributes['iid'])
     username = get_username(mr_json)
 
-    if not has_changed_changelog(project_id, iid, only_md_json=True):
-        if has_changed_changelog(project_id, iid, only_md_json=False):
+    if not has_changed_changelog(project_id, iid, project_name, only_md=True):
+        if has_changed_changelog(project_id, iid, project_name, only_md=False):
             msg = NO_MD_CHANGELOG
         else:
             msg = MSG_MISSING_CHANGELOG
@@ -231,14 +231,21 @@ def check_labels_and_weight(push: dict, branch_name: str) -> NoReturn:
 
 
 # @ehorvat: I believe this should be in a utils as it depends on gitlab
-def has_changed_changelog(project_id: int, iid: int, only_md_json: bool):
+def has_changed_changelog(project_id: int, iid: int, project_name, only_md: bool):
     changes = get_mr_changes(project_id, iid)
     changed_files = get_changed_files(changes)
     for filename in changed_files:
         if filename.startswith('CHANGELOG'):
-            valid_extension = filename.endswith('.md') or filename.endswith('.json')
-            if not only_md_json or valid_extension:
+            valid_ext = config[project_name]['changelog_filetype'] if 'changelog_filetype' in config[project_name] \
+                                                                   else '.md'
+            if not only_md or filename.endswith(valid_ext):
                 return True
+            else:
+                if 'changelog_exceptions' in config[project_name]:
+                    _, file_name = os.path.split(filename)
+                    if file_name in config[project_name]['changelog_exceptions']:
+                        return True
+
     return False
 
 
