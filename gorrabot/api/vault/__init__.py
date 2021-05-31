c21da7178762a731d063bfd653953504eba0f979
Ciro Goyeneche <soycirog@gmail.com>
2021-05-31 14:10:33 -0300

    [ADD] SECRET_NAME constant

--- gorrabot/api/vault/__init__.py
+++ gorrabot/api/vault/__init__.py
@@ -4,5 +4,6 @@ import os
 FARADAY_VAULT_SERVER = "https://tluav-lb.***REMOVED***.com/"
 ROLE_ID = os.environ['ROLE_ID']
 SECRET_ID = os.environ['SECRET_ID']
+SECRET_NAME = os.environ['SECRET_NAME']
 
 from .utils import get_secret
