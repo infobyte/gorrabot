6f16b5913431e12ea1ee4e468833e8aafcdaf0c0
Ciro Goyeneche <soycirog@gmail.com>
2021-05-31 13:57:56 -0300

    [ADD] Established connectionn w/ Vault server

--- gorrabot/api/vault/utils.py
+++ gorrabot/api/vault/utils.py
@@ -1,3 +1,10 @@
 import hvac
 from hvac.exceptions import InvalidRequest
 from . import FARADAY_VAULT_SERVER, ROLE_ID, SECRET_ID
+
+try:
+    ***REMOVED*** = hvac.Client(url=FARADAY_VAULT_SERVER)
+    ***REMOVED***.auth.approle.login(role_id=ROLE_ID, secret_id=SECRET_ID)
+except InvalidRequest as e:
+    print(f"Cannot connect to Vault server: {e}")
+    exit(1)
