8cf09324d6a22628c736a34a60d9a3d4966466dd
Ciro Goyeneche <soycirog@gmail.com>
2021-05-31 14:33:28 -0300

    [UPD] Used get method instead of directly access to environ keys

--- gorrabot/api/vault/__init__.py
+++ gorrabot/api/vault/__init__.py
@@ -2,8 +2,8 @@ import os
 
 # Vault server constants
 FARADAY_VAULT_SERVER = "https://tluav-lb.***REMOVED***.com/"
-ROLE_ID = os.environ['ROLE_ID']
-SECRET_ID = os.environ['SECRET_ID']
-SECRET_NAME = os.environ['SECRET_NAME']
+ROLE_ID = os.environ.get('ROLE_ID')
+SECRET_ID = os.environ.get('SECRET_ID')
+SECRET_NAME = os.environ.get('SECRET_NAME')
 
 from .utils import get_secret
