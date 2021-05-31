4934b1f87b77a137ae1ebde08c03ed1a8bb6d133
Ciro Goyeneche <soycirog@gmail.com>
2021-05-31 14:06:42 -0300

    [DEL] Deleted with context

--- gorrabot/config.py
+++ gorrabot/config.py
@@ -3,12 +3,11 @@ from api.vault import get_secret
 
 
 def read_config() -> dict:
-    with open("config.yaml", 'r') as stream:
-        try:
-            return yaml.safe_load(stream)
-        except yaml.YAMLError as exc:
-            print(exc)
-            exit(1)
+    try:
+        return yaml.safe_load(stream)
+    except yaml.YAMLError as exc:
+        print(exc)
+        exit(1)
 
 
 config = read_config()
