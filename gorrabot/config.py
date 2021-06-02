25e8b120222d2a6eff38d8544ba99f49ea019a96
Ciro Goyeneche <soycirog@gmail.com>
2021-06-02 16:01:21 -0300

    [ADD] Created load_yaml() function

--- gorrabot/config.py
+++ gorrabot/config.py
@@ -2,7 +2,17 @@ import yaml
 from gorrabot.api.vault import SECRET_NAME, GORRABOT_CONFIG_FILE, get_secret
 
 
+def load_yaml(data):
+    try:
+        return yaml.safe_load(data)
+    except yaml.YAMLError as exc:
+        print(exc)
+        exit(1)
+
+
 def read_config(secret) -> dict:
+    if secret.endswith('.yaml'):
+        secret = open(secret, 'r')
     try:
         return yaml.safe_load(secret)
     except yaml.YAMLError as exc:
