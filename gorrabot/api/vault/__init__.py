7806eaa0acd8926530dd08c11f589baae898ebac
Ciro Goyeneche <soycirog@gmail.com>
2021-06-02 17:02:16 -0300

    [UPD] Converted GORRABOT_CONFIG_FILE to dict

--- gorrabot/api/vault/__init__.py
+++ gorrabot/api/vault/__init__.py
@@ -5,7 +5,9 @@ FARADAY_VAULT_SERVER = "https://tluav-lb.***REMOVED***.com/"
 ROLE_ID = os.environ.get('ROLE_ID')
 SECRET_ID = os.environ.get('SECRET_ID')
 SECRET_NAME = os.environ.get('SECRET_NAME')
-GORRABOT_CONFIG_FILE = os.environ.get('GORRABOT_CONFIG_FILE')
-
+GORRABOT_CONFIG_FILE = {
+    'path': os.environ.get('GORRABOT_CONFIG_FILE'),
+    'path_regex': '^(/[^/ ]*)+/?$'
+}
 
 from .utils import get_secret
