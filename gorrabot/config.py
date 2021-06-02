e8629d1040de635148292906612f64e78adbb0b1
Ciro Goyeneche <soycirog@gmail.com>
2021-06-02 16:53:05 -0300

    [UPD] Verify secret is a valid file

--- gorrabot/config.py
+++ gorrabot/config.py
@@ -1,4 +1,5 @@
 import yaml
+import re
 from gorrabot.api.vault import SECRET_NAME, GORRABOT_CONFIG_FILE, get_secret
 
 
@@ -11,14 +12,20 @@ def load_yaml(data):
 
 
 def read_config(secret) -> dict:
-    if secret.endswith('.yaml'):
-        secret = open(secret, 'r')
-    try:
-        return yaml.safe_load(secret)
-    except yaml.YAMLError as exc:
-        print(exc)
-        exit(1)
+    if re.match(GORRABOT_CONFIG_FILE['path_regex'], secret):  # must be an absolute path
+        if not secret.endswith('.yaml'):
+            print("Invalid GORRABOT_CONFIG_FILE: It must be a .yaml file")
+            exit(1)
+
+        try:
+            with open(secret, 'r') as stream:
+                return load_yaml(stream)
+        except FileNotFoundError:
+            print("File not found")
+            exit(1)
+
+    return load_yaml(secret)
 
 
-secret = get_secret(SECRET_NAME) if SECRET_NAME else GORRABOT_CONFIG_FILE
+secret = get_secret(SECRET_NAME) if SECRET_NAME else GORRABOT_CONFIG_FILE['path']
 config = read_config(secret)
