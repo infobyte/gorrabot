15e7c3531a8df769bf6039cf54d076fbf7e128b4
Ciro Goyeneche <soycirog@gmail.com>
2021-06-04 13:58:00 -0300

    [ADD] Created GorrabotTimer class

--- gorrabot/timer.py
+++ gorrabot/timer.py
@@ -1,1 +1,25 @@
 from threading import Timer
+
+
+class GorrabotTimer:
+    def __init__(self, function, interval):
+        self._timer = None
+        self.function = function
+        self.interval = interval
+        self.is_running = False
+        self.start()
+
+    def _run(self):
+        self.is_running = False
+        self.start()
+        self.function()
+
+    def start(self):
+        if not self.is_running:
+            self._timer = Timer(self.interval, self._run)
+            self._timer.start()
+            self.is_running = True
+
+    def stop(self):
+        self._timer.cancel()
+        self.is_running = False
