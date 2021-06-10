4562b52c132d912dac272bf63cce23d9369d4826
Ciro Goyeneche <soycirog@gmail.com>
2021-06-10 16:43:10 -0300

    [ADD] Created check_can_send_slack_messages function to check if project can send messages

--- gorrabot/api/slack/messages.py
+++ gorrabot/api/slack/messages.py
@@ -1,7 +1,20 @@
 import os
 
+from gorrabot.api.gitlab.projects import get_project_name
 from gorrabot.api.slack import slack_session, SLACK_API_PREFIX
 from gorrabot.config import config
+
+
+def check_can_send_slack_messages(project_id: int):
+    """ Checks the send_message_to_slack config param is set """
+
+    project_name = get_project_name(project_id)
+
+    send_message_to_slack = False
+    if project_name in config['projects']:
+        send_message_to_slack = config['projects'][project_name].get('send_message_to_slack', False)
+
+    return send_message_to_slack
 
 
 def send_message_to_user(slack_user: str, text: str, slack_users_data: dict):
@@ -22,12 +35,12 @@ def send_message_to_user(slack_user: str, text: str, slack_users_data: dict):
         return res
 
 
-def send_message_to_channel(slack_channel: str, text: str, project_name: str):
+def send_message_to_channel(slack_channel: str, text: str, project_id: int):
 
-    send_message_to_slack = config['projects'][project_name].get('send_message_to_slack', None)
+    can_send_message = check_can_send_slack_messages(project_id)
 
-    if not send_message_to_slack:
-        print(f"'send_message_to_slack' has not been set in {project_name} project")
+    if not can_send_message:
+        # project cannot send message to Slack
         return
 
     params = {
@@ -39,10 +52,10 @@ def send_message_to_channel(slack_channel: str, text: str, project_name: str):
     return res
 
 
-def send_message_to_error_channel(text: str, project_name: str):
-    send_message_to_channel("#***REMOVED***-notification", text, project_name)
+def send_message_to_error_channel(text: str, project_id: int):
+    send_message_to_channel("#***REMOVED***-notification", text, project_id)
 
 
-def send_debug_message(text: str):
+def send_debug_message(text: str, project_id: int):
     if 'DEBUG' in os.environ:
-        send_message_to_channel("***REMOVED***", text)  # ***REMOVED*** ID
+        send_message_to_channel("***REMOVED***", text, project_id)  # ***REMOVED*** ID
