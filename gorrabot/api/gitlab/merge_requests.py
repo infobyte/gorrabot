a9dbcee99042839e87b6f38c5a2746fe5b3bc157
Ciro Goyeneche <soycirog@gmail.com>
2021-06-10 11:07:23 -0300

    DEBUG message added

--- gorrabot/api/gitlab/merge_requests.py
+++ gorrabot/api/gitlab/merge_requests.py
@@ -86,7 +86,8 @@ def get_related_merge_requests(project_id: int, issue_iid: int):
 def comment_mr(project_id: int, iid: int, body: str, can_be_duplicated=True, min_time_between_comments=None):
     project_name = get_project_name(project_id)
     comment_mr = config['projects'][project_name].get('comment_mr', None)
-    if not (comment_mr or isinstance(comment_mr, bool)):
+    if not comment_mr:
+        print("NO TENES HABILITADO MR")
         return
     if not can_be_duplicated:
         # Ugly hack to drop user mentions from body
