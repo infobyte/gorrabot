3c5b280d2d960da3a18a2aa835bb9165cd53a64b
Gonzalo Martinez <gmartinez@faradaysec.com>
2022-11-22 19:05:30 +0000

    Update constants.py

--- gorrabot/constants.py
+++ gorrabot/constants.py
@@ -14,7 +14,7 @@ MSG_MISSING_CHANGELOG = (
 )
 MSG_CHANGELOG_DOSENT_PREFIX = "Los CHANGELOGS deberian empezar con [ADD], [MOD], [FIX], [DEL]" \
                                           "acorde al cambio y parece que tu changelog no lo esta haciendo, por favor" \
-                                          "agregalo cuanod puedas"
+                                          "agregalo cuando puedas"
 NO_VALID_CHANGELOG_FILETYPE = (
     'El fichero que se creó en el directorio `CHANGELOG` no tiene extensión '
     '`{changelog_filetype}` por lo que no va a ser tomado en cuenta por el sistema de '
